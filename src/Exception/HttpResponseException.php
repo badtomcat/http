c1ba9f8ddc99ee7b7230ee94e86646cac71c9225
tiananwei <awei.tian@qq.com>
2017-12-04 13:15:04 +0800

    add 2 exceptions

---
+++ src/Exception/HttpResponseException.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tian\Http\Exception;
+
+use RuntimeException;
+use Tian\Http\Response;
+
+class HttpResponseException extends RuntimeException
+{
+    /**
+     * The underlying response instance.
+     *
+     * @var Response
+     */
+    protected $response;
+
+    /**
+     * Create a new HTTP response exception instance.
+     *
+     * @param  Response  $response
+     * @return void
+     */
+    public function __construct(Response $response)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * Get the underlying response instance.
+     *
+     * @return Response
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+}
---
+++ src/Exception/PostTooLargeException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tian\Http\Exception;
+
+use Exception;
+
+class PostTooLargeException extends Exception
+{
+    //
+}
